b60ca4e85a18a4880a99d173d00ad1358b19bf7d
Conor Carmichael <conorbcarmichael@gmail.com>
2020-09-23 13:05:25 -0400

    Slight updates

--- post_process_pcd.py
+++ post_process_pcd.py
@@ -44,7 +44,7 @@ def rotate_points(points):
     return rotated
 
 
-def rescale(points, scale_fact_x=3.25, scale_fact_y=3.25):
+def rescale(points, scale_fact_x=3.75, scale_fact_y=3.60):
     resc = []
     for point in points:
         p = [float(v) for v in point]
